9ff5ece4dd51862fe0d83def7665bba09207c889
Lucas McClean <LucksDeveloper@gmail.com>
2025-03-01 22:11:42 -0500

    fix: changed lazy.lua to setup plugins properly

--- nvim/lua/plugins/comment.lua
+++ nvim/lua/plugins/comment.lua
@@ -1,22 +1,24 @@
 return {
-	"numToStr/Comment.nvim",
-	opts = {
-		toggler = {
-			line = "gcc",
-			block = "gbc",
-		},
-		opleader = {
-			line = "gc",
-			block = "gb",
-		},
-		extra = {
-			above = "gcO",
-			below = "gco",
-			eol = "gcA",
-		},
-		mappings = {
-			basic = true,
-			extra = true,
+	{
+		"numToStr/Comment.nvim",
+		opts = {
+			toggler = {
+				line = "gcc",
+				block = "gbc",
+			},
+			opleader = {
+				line = "gc",
+				block = "gb",
+			},
+			extra = {
+				above = "gcO",
+				below = "gco",
+				eol = "gcA",
+			},
+			mappings = {
+				basic = true,
+				extra = true,
+			},
 		},
 	},
 }
--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -1,18 +1,20 @@
 return {
-	"nvim-telescope/telescope.nvim",
-	event = "VimEnter",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		{
-			"nvim-telescope/telescope-fzf-native.nvim",
-			build = "make",
-			cond = function()
-				return vim.fn.executable("make") == 1
-			end,
+	{
+		"nvim-telescope/telescope.nvim",
+		event = "VimEnter",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			{
+				"nvim-telescope/telescope-fzf-native.nvim",
+				build = "make",
+				cond = function()
+					return vim.fn.executable("make") == 1
+				end,
+			},
 		},
+		config = function()
+			require("telescope").setup({})
+			pcall(require("telescope").load_extension, "fzf")
+		end,
 	},
-	config = function()
-		require("telescope").setup({})
-		pcall(require("telescope").load_extension, "fzf")
-	end,
 }
